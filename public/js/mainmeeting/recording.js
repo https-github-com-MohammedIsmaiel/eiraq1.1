a557be14bf7f22103bfa99523ede6acac62641a3
Eng-Muhammed-Samy <eng_muhamed_samy@yahoo.com>
2021-05-08 00:33:17 +0200

    add record and timer

--- public/js/mainmeeting/recording.js
+++ public/js/mainmeeting/recording.js
@@ -1,18 +1,26 @@
-const recordScreen = document.getElementById('recordScreen')
+/** @format */
+
+const recordScreen = document.getElementById('recordScreen');
+let isRecording = false;
+
+var options = {
+	disableLogs: true,
+	frameInterval: 1,
+	mimeType: 'video/webm',
+};
+
+var recorder = new MultiStreamRecorder(allStreams, options);
 
 recordScreen.addEventListener('click', () => {
-    if (isRecording === true) {
-        var recorder = connection.recorder;
-        if (!recorder) return alert('No recorder found.');
-        recorder.stopRecording(() => {
-            var blob = recorder.getBlob();
-            invokeSaveAsDialog(blob,'*.webm');
+	if (isRecording === true) {
+		recorder.stop(function (blob) {
+			// video.src = URL.createObjectURL(blob);
+			invokeSaveAsDialog(blob, '*.webm');
+		});
 
-            connection.recorder = null;
-        })
-        isRecording = false
-    }else{
-        startRecording()
-    }
-
-})
+		isRecording = false;
+	} else {
+		recorder.record();
+		isRecording = true;
+	}
+});

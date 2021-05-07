297dc42410070fdc04318b4a50d03c2510977faa
Eng-Muhammed-Samy <eng_muhamed_samy@yahoo.com>
2021-05-07 02:36:57 +0200

    edit endForAll functionalty

--- public/js/mainmeeting/leave.js
+++ public/js/mainmeeting/leave.js
@@ -2,26 +2,30 @@
 const leave = document.querySelector('#leave')
 const endForAll = document.querySelector('#endForAll')
 
+
+
 leave.addEventListener('click', (e) => {
     connection.closeSocket()
     window.location.replace('/profile');
 })
 
-endForAll.addEventListener('click', (e) => {
-    // disconnect with all users
-    connection.getAllParticipants().forEach(function (pid) {
-        connection.disconnectWith(pid);
-    });
-    // stop all local cameras
-    connection.attachStreams.forEach(function (localStream) {
-        localStream.stop();
-    });
-    // close socket.io connection
-    connection.closeSocket();
-    connection.autoCloseEntireSession = true
-    socket.emit('endForAll')
-    window.location.replace('/profile');
-})
+if (endForAll != null) {
+    endForAll.addEventListener('click', (e) => {
+        // disconnect with all users
+        connection.getAllParticipants().forEach(function (pid) {
+            connection.disconnectWith(pid);
+        });
+        // stop all local cameras
+        connection.attachStreams.forEach(function (localStream) {
+            localStream.stop();
+        });
+        // close socket.io connection
+        connection.closeSocket();
+        connection.autoCloseEntireSession = true
+        socket.emit('endForAll')
+        window.location.replace('/profile');
+    })
+}
 connection.onEntireSessionClosed = function (event) {
     console.log('session closed');
     window.location.replace('/profile');

c3515b3b692c40045a41c5fda7434811675dbb81
yasminekamal <yassminkamal92@gmail.com>
2021-07-02 13:39:11 +0200

    yass

--- models/googleDrive.js
+++ models/googleDrive.js
@@ -1,28 +1,9 @@
 
 
 const fs = require('fs');
-const { google } = require('googleapis');
-const CLIENT_ID = '985573331622-9mddg0qss828jtavbervj05akm4ah38i.apps.googleusercontent.com';
-const CLIENT_SECRET = 'k3qlwPGczaPnlK3hGvJV-Emz';
-const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
+var connection_drive = require('./init_google_connection');
+var drive=connection_drive();
 
-const REFRESH_TOKEN = '1//04j8Y28dmaQ2XCgYIARAAGAQSNwF-L9Ir644owAq35q-BXXbkTgGXpiGUdUfNmI_0Tij950GJBSacr0hh6c6UucKFFwjtrj-wzXI';
-
-
-
-
-    const oauth2Client = new google.auth.OAuth2(
-        CLIENT_ID,
-        CLIENT_SECRET,
-        REDIRECT_URI
-      );
-      
-      oauth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
-      
-      const drive = google.drive({
-        version: 'v3',
-        auth: oauth2Client,
-      });
 
 /* 
 filepath which needs to be uploaded
--- routes/profile.route.js
+++ routes/profile.route.js
@@ -4,29 +4,8 @@ const check = require("express-validator").check;
 const path = require('path');
 const profileController = require('../controllers/profile.controller');
 const router = require('express').Router();
-const { google } = require('googleapis');
-const CLIENT_ID = '985573331622-9mddg0qss828jtavbervj05akm4ah38i.apps.googleusercontent.com';
-const CLIENT_SECRET = 'k3qlwPGczaPnlK3hGvJV-Emz';
-const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
-
-const REFRESH_TOKEN = '1//04j8Y28dmaQ2XCgYIARAAGAQSNwF-L9Ir644owAq35q-BXXbkTgGXpiGUdUfNmI_0Tij950GJBSacr0hh6c6UucKFFwjtrj-wzXI';
-
-
-
-
-    const oauth2Client = new google.auth.OAuth2(
-        CLIENT_ID,
-        CLIENT_SECRET,
-        REDIRECT_URI
-      );
-      
-      oauth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
-      
-      const drive = google.drive({
-        version: 'v3',
-        auth: oauth2Client,
-      });
-    
+var connection_drive = require('../models/init_google_connection');
+var drive=connection_drive();
 var multer = require('multer')
 var GoogleDriveStorage = require('multer-google-drive');
 const storage = GoogleDriveStorage({

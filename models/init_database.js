c0341674dbb520433a438bf28794e450c56842a6
Eng-Muhammed-Samy <eng_muhamed_samy@yahoo.com>
2021-07-07 14:27:02 +0200

    edit database config

--- models/init_database.js
+++ models/init_database.js
@@ -2,11 +2,11 @@ const { Pool } = require('pg');
 
 const connection = new Pool({
 	host: 'ec2-52-5-1-20.compute-1.amazonaws.com',
-	user: 'zngsfxbaerzzgo',
+	user: 'aeaubsshaayzwa',
 	password:
-		'75e96c83ec0f50d0bc9e0cc3a94c13d9b07ad4b52e8ad562ad3770c866b54ad8',
+		'4a2d34caf8e4d3903b6c7d453014c581abeac214a1f8356b617aaafecc03741e',
 	port: '5432',
-	database: 'deqajgslo5pt7n',
+	database: 'd48j1jvrg9tvit',
 	ssl: {
 		rejectUnauthorized: false,
 	},
